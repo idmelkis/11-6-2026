77a6d734376117b1ad9a9737252984101dd3a85c
Ingmārs Meļķis <idmelkis@edu.riga.lv>
2026-09-02 09:28:11 +0300

    02.09 nodarbība

---
+++ 1_nodarbiba.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+# Komentāri: CTRL + /
+# print("11")
+# print("22!")
+# Komentārs 
+print("333!")
+# Mainīgais - dati kas tiek glabāti atmiņā ar kaut kādu nosaukumu
+ievadītais_teksts = input("Ievade: ")
+
+# E-klases login -
+# teksts, skaitlis, loģiskie mainīgie, saraksti
+lietotājvārds = ""
+parole = ""
+# Personas kods
+# Parole
+# E-klasē priekšmets -
+# Skolotājs
+# Kabinets
+# Laiki
+# Nosaukums
